2942df56677cb69bf736f142812e9146af075278
rsuazo <rsuazo2@gmail.com>
2023-07-08 15:19:00 -0500

    completed app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Counter from './Counter'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        mount: true,
+        ignoreProp: 0,
+        seed: 40,
+        showErrorComponent: false
+    }
+
+    this.mountCounter = () => {this.setState({mount: true})}
+    this.unmountCounter = () => {this.setState({mount: false})}
+
+    this.ignoreProp = () => {this.setState({ignoreProp: Math.random()})}
+    this.seedGenerator = () => {this.setState({seed: Number.parseInt(Math.random() * 100)})}
+    this.toggleError = () => {this.setState({showErrorComponent: !this.state.showErrorComponent})}
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.mountCounter} disabled={this.state.mount}>Mount Counter</button>
+        <button onClick={this.unmountCounter} disabled={!this.state.mount}>Unmount Counter</button>
+        <button onClick={this.ignoreProp}>Ignore Prop</button>
+        <button onClick={this.seedGenerator}>Generate Seed</button>
+        <button onClick={this.toggleError}>Toggle Error</button>
+
+        {this.state.mount ? 
+            <Counter 
+                ignoreProp = {this.state.ignoreProp}
+                seed = {this.state.seed}
+                showErrorComponent = {this.state.showErrorComponent}
+            /> : 
+            null }
+      </div>
+    );
+  }
 }
 
 export default App;
